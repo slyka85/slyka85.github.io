c0676c573b0f37783257634db9f85921b7267cd2
Anya Melnyk <gannasachakov@gmail.com>
2016-03-06 16:40:04 -0500

    added logic to detect media query for scrolling calculations

--- Selus/assets/js/page-scrolling.js
+++ Selus/assets/js/page-scrolling.js
@@ -6,4 +6,19 @@ $(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
+// IF scrolling on MOBILE, do not take into count the height of navigation bar
+var mql = window.matchMedia("screen and (max-width: 480px)");
+if (mql.matches){ // if media query matches
+ console.log("Window is 480 or smaller");
+     $('a.page-scroll').click(function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+}
+
+
 });
