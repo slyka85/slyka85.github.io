8a1b5f062c6640dd992fd990102b7d4a786c7e4d
Anya Melnyk <gannasachakov@gmail.com>
2016-03-06 21:26:37 -0500

    carousel responsive

--- Selus/assets/js/carousel.js
+++ Selus/assets/js/carousel.js
@@ -1,10 +1,19 @@
 
-$(document).ready(function(){
-  $('.carousel-wrapper').slick({
+
+  $('.responsive').slick({
 	  dots: true,
 	  infinite: true,
 	  speed: 300,
-	  slidesToShow: 1
+	  slidesToShow: 1,
+	    responsive: [
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
   });
-});
+
 	
